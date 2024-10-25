953becd0655e3e36237551d007c37bcae85b1c7d
wiwe <wiweurdaneta@gmail.com>
2024-10-25 14:56:29 -0400

    Final commit

--- transferencias.cpp
+++ transferencias.cpp
@@ -31,28 +31,24 @@ void leerArchivoBin() {
     float totalTransferidoInvalido = 0.0, totalTransferidoValido = 0.0;
     float sumaTrimestre1 = 0.0, sumaTrimestre2 = 0.0, sumaTrimestre3 = 0.0, sumaTrimestre4 = 0.0;
 
-    // while (archivoBin.read(reinterpret_cast<char*>(&registro), sizeof(Registro))) {
-    //     cout << registro.id << registro.year << registro.month << registro.day << registro.Cedula_cuenta << endl;
-    // }
-
     while (archivoBin.read(reinterpret_cast<char*>(&registro), sizeof(Registro))) {
         if (registro.year >= 2010 && registro.year <= 2015) {
             if (registro.Estado_de_transferencia == 0){
                 contadorTotal++;
                 totalTransferidoInvalido += registro.Cantidad_a_transferir;
-            if (registro.month >= 0 && registro.month <= 3) {
-                trimestre1++;
-                sumaTrimestre1 += registro.Cantidad_a_transferir;
-            } else if (registro.month >= 4 && registro.month <= 6) {
-                trimestre2++;
-                sumaTrimestre2 += registro.Cantidad_a_transferir;
-            } else if (registro.month >= 7 && registro.month <= 9) {
-                trimestre3++;
-                sumaTrimestre3 += registro.Cantidad_a_transferir;
-            } else if (registro.month >= 10 && registro.month <= 12) {
-                trimestre4++;
-                sumaTrimestre4 += registro.Cantidad_a_transferir;
-            }
+                if (registro.month >= 0 && registro.month <= 3) {
+                    trimestre1++;
+                    sumaTrimestre1 += registro.Cantidad_a_transferir;
+                } else if (registro.month >= 4 && registro.month <= 6) {
+                    trimestre2++;
+                    sumaTrimestre2 += registro.Cantidad_a_transferir;
+                } else if (registro.month >= 7 && registro.month <= 9) {
+                    trimestre3++;
+                    sumaTrimestre3 += registro.Cantidad_a_transferir;
+                } else if (registro.month >= 10 && registro.month <= 12) {
+                    trimestre4++;
+                    sumaTrimestre4 += registro.Cantidad_a_transferir;
+                }
             } else if (registro.Estado_de_transferencia == 1){
                 totalTransferidoValido += registro.Cantidad_a_transferir;
             }
@@ -82,32 +78,32 @@ void leerArchivoBin() {
 }
 
 int main() {
-    // Registro registro;
-    // ifstream archivoTxt("Data.txt");
-    // ofstream archivoBin("archivo.bin", ios::out | ios::binary);
+    Registro registro;
+    ifstream archivoTxt("Data.txt");
+    ofstream archivoBin("archivo.bin", ios::out | ios::binary);
 
-    // if (!archivoTxt) {
-    //     cout << "Error al abrir Data.txt" << endl;
-    //     return 0;
-    // }
+    if (!archivoTxt) {
+        cout << "Error al abrir Data.txt" << endl;
+        return 0;
+    }
 
-    // if (!archivoBin) {
-    //     cout << "Error al crear archivo.bin" << endl;
-    //     return 0;
-    // }
+    if (!archivoBin) {
+        cout << "Error al crear archivo.bin" << endl;
+        return 0;
+    }
 
-    // while (archivoTxt >> registro.id 
-    //              >> registro.year >> registro.month >> registro.day
-    //              >> registro.Cedula_cuenta >> registro.Cedula_cuenta_a_transferir
-    //              >> registro.Cantidad_en_cuenta >> registro.Cantidad_a_transferir
-    //              >> registro.Estado_de_transferencia) {
-    //     archivoBin.write(reinterpret_cast<char*>(&registro), sizeof(Registro));
-    // }
+    while (archivoTxt >> registro.id 
+                 >> registro.year >> registro.month >> registro.day
+                 >> registro.Cedula_cuenta >> registro.Cedula_cuenta_a_transferir
+                 >> registro.Cantidad_en_cuenta >> registro.Cantidad_a_transferir
+                 >> registro.Estado_de_transferencia) {
+        archivoBin.write(reinterpret_cast<char*>(&registro), sizeof(Registro));
+    }
 
-    // archivoTxt.close();
-    // archivoBin.close();
+    archivoTxt.close();
+    archivoBin.close();
 
-    // cout << "Datos pasados al archivo binario." << endl;
+    cout << "Datos pasados al archivo binario." << endl;
     leerArchivoBin();
 
     return 0;
